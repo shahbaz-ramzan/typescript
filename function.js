f0fc0a9273d5dcdb68b5527630d56f9d4571269a
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-10-26 19:03:20 +0500

    latest function code in ts

--- function.js
+++ function.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// function for add number  
 function addTwo(num) {
-    num.toUpperCase();
     return num + 2;
+    // return "shahbaz"
 }
 console.log(addTwo(5));
 // uppercase
@@ -17,5 +18,22 @@ function signUpUser(name, email, isPaid) {
 signUpUser("shahbaz", "shahbaz@gmail.com", false);
 // loggedin
 function logedInUser(name, email, isLoggedIn) {
+    if (isLoggedIn === void 0) { isLoggedIn = false; }
 }
-logedInUser("shahbaz", "shahbaz@gmail.com", true);
+logedInUser("shahbaz", "shahbaz@gmail.com");
+// function returnValue(myVal:number):boolean{
+//    if(myVal>5){
+//     return true
+//    }
+//    return "200 ok"
+// }
+// getValue
+var getHello = function (s) {
+    return s;
+};
+var r = getHello("Hello");
+console.log(r);
+var heros = ["thor", "spiderman", "ironman"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
--- function.ts
+++ function.ts
@@ -1,6 +1,8 @@
-function addTwo(num){
-num.toUpperCase()
+
+// function for add number  
+function addTwo(num:number):number{
 return num+2
+// return "shahbaz"
 }
 console.log(addTwo(5))
 
@@ -29,10 +31,35 @@ signUpUser("shahbaz","shahbaz@gmail.com",false)
 
 // loggedin
 
-function logedInUser(name:String,email:String,isLoggedIn:Boolean){
+function logedInUser(name:String,email:String,isLoggedIn:Boolean=false){
 
 }
 
-logedInUser("shahbaz","shahbaz@gmail.com",true)
+logedInUser("shahbaz","shahbaz@gmail.com")
+
+
+// function returnValue(myVal:number):boolean{
+//    if(myVal>5){
+//     return true
+//    }
+//    return "200 ok"
+// }
+
+
+// getValue
+
+const getHello=(s:string):string =>{
+   return s
+}
+
+let r= getHello("Hello")
+console.log(r)
+
+const heros=["thor","spiderman","ironman"]
+
+
+heros.map((hero):string=>{
+    return `hero is ${hero}`
+})
 
 
