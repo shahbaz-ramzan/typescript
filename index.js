7130e1a20c0328f09092a2fc73e94c8d1c41dc41
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-10-30 12:42:11 +0500

    get the string length

---
+++ index.js
@@ -0,0 +1,10 @@
+// function define
+var sum = function (a, b) {
+    return a + b;
+};
+// function call
+console.log(sum(5, 10));
+// string length
+var myName = "shahbaz";
+var stringLength = myName.length;
+console.log(stringLength);
--- index.ts
+++ index.ts
@@ -7,3 +7,12 @@ const sum = (a:number,b:number):number=>{
 
 // function call
 console.log(sum(5,10))
+
+
+// string length
+
+
+let myName:string="shahbaz"
+let stringLength:number=myName.length
+console.log(stringLength)
+
